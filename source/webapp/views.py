f9ed30220236ee7c325a6b5af5336057dda47389
Malik <imenov-a@mail.ru>
2020-12-28 06:33:25 +0600

    json запросы, на клиентсий сервер

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,12 +1,18 @@
 import json
 from datetime import datetime
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt              # Декоратор @csrf_exempt обозначает, что для этого представления Django не будет проверять наличие csrf-токена при POST-запросах.
 def json_echo_view(request, *args, **kwargs):
+    data = None
+    if request.body:
+        data = json.loads(request.body)
     answer = {
         'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         'method': request.method,
+        'content': data
     }
 
     answer_as_json = json.dumps(answer)
